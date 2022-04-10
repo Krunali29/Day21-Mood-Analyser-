22fe64ef64d6fbc1f6a5b85a7ace153b1fd0a748
Krunali <krunalilole29@gmail.com>
2022-04-10 23:12:24 +0530

    Adding UC3 To Inform User For Invalid Mood

--- src/com/bridgelabz/MoodAnalyser/MoodAnalyserException.java
+++ src/com/bridgelabz/MoodAnalyser/MoodAnalyserException.java
@@ -1,11 +1,15 @@
 package com.bridgelabz.MoodAnalyser;
 
 public class MoodAnalyserException extends Exception {
-    public MoodAnalyserException(String message)
+    public enum ExceptionType{
+        ENTERED_NULL,ENTERED_EMPTY;
+    }
+    ExceptionType type;
+    public MoodAnalyserException(ExceptionType type,String message)
     {
         super(message);
+        this.type=type;
     }
 
 }
-
 
